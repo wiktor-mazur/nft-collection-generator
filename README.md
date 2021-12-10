a8c4759c74ca8b19b909cfc4aab8b5dfc7f4c8e2
Wiktor Mazur <me@wiktormazur.pl>
2021-12-10 16:16:02 +0100

    Implements the script

--- README.md
+++ README.md
@@ -1,4 +1,49 @@
 # NFT collection generator
-___
 
 Simple Python script that allows you to mix arbitrary number of layers into a collection of unique images.
+
+## Installation
+1. Make sure you have Python3 installed.
+2. Create venv:
+    ```bash
+    python -m venv env
+    ```
+3. Activate venv:
+    ```bash
+    source env/bin/activate
+    ```
+4. Install requirements:
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+## Usage
+1. Configure `config.yaml`.
+2. Prepare proper files structure.
+3. Run the script:
+    ```bash
+    python main.py
+    ```
+4. The result will be generated in `./YOUR_COLLECTION_NAME/result/`.
+
+This repo comes with an example configuration out-of-the-box, so you should be able to run `python main.py` right after
+you install all required dependencies.
+
+## Configuration
+
+```yaml
+collections:
+  - name: example
+    layers:
+      - background
+      - shape
+      - text
+```
+The top-level of the YAML is a list of collections to generate. For each collection you need to specify a name
+(which corresponds to a collection's directory that has to be created in the script's root location) and a list of
+layers. Each layer is simply a directory name that will need to be created inside the collection's dir. In each layer's
+directory you need to put the images that will be used for generation.
+
+## Limitations
+- tested image formats: png
+- every image in the layers' dirs has to be the exact same dimensions
--- main.py
+++ main.py
@@ -1,1 +1,75 @@
-print("Hello world!")
+from PIL import Image
+from os import listdir, makedirs, path as os_path
+from shutil import rmtree
+from itertools import product
+from hashlib import md5
+from yaml import safe_load as yaml_safe_load, YAMLError
+
+
+def get_layer_path(collection_name, layer_name):
+    return './' + collection_name + '/' + layer_name + '/'
+
+
+def prepare_clean_directory(path):
+    if os_path.exists(path):
+        rmtree(path)
+    makedirs(path)
+
+
+def get_layers_images(collection):
+    """Returns a two-dimensional array.
+    First-level is an array of layers
+    Second-level is an array of images in the particular array
+    """
+    layers_images = []
+    for layer in collection['layers']:
+        layers_images.append(listdir(get_layer_path(collection['name'], layer)))
+
+    return layers_images
+
+
+def get_config():
+    with open('config.yaml', "r") as stream:
+        try:
+            return yaml_safe_load(stream)
+        except YAMLError:
+            print('Could not read config.yaml - make sure it exists and is valid.')
+            exit()
+
+
+def main():
+    config = get_config()
+
+    for collection in config['collections']:
+        layers_images = get_layers_images(collection)
+        layers_cartesian_product = list(product(*layers_images))
+        result_dir = './' + collection['name'] + '/result/'
+        prepare_clean_directory(result_dir)
+
+        number_of_unique_sets = len(layers_cartesian_product)
+
+        decision = input('You are about to generate ' + str(number_of_unique_sets) + ' unique images in the "'
+                         + collection['name'] + '" collection. Do you want to continue? (Y/n) ')
+
+        if decision != 'y' and decision != 'Y':
+            exit()
+        else:
+            print('Processing the "' + collection['name'] + '" collection...\n')
+
+        for unique_set in layers_cartesian_product:
+            background_layer_img = None
+            for index, image_name in enumerate(unique_set):
+                image_path = get_layer_path(collection['name'], collection['layers'][index]) + image_name
+
+                if index == 0:
+                    background_layer_img = Image.open(image_path).convert('RGBA')
+                else:
+                    current_layer_img = Image.open(image_path).convert('RGBA')
+                    background_layer_img.paste(current_layer_img, (0, 0), current_layer_img)
+
+            result_name = md5(bytes('-'.join(unique_set), 'utf-8')).hexdigest()
+            background_layer_img.save(result_dir + result_name + '.png', format='png')
+
+
+if __name__ == '__main__':
+    main()
